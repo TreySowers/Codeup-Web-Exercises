dd1883ced4be55fe3d6784b9785236720f0935d6
George Sowers III <Trey@Georges-MacBook-Pro.local>
2015-10-05 09:43:52 -0500

    While Loops

---
+++ public/js/if_else.js
@@ -0,0 +1,52 @@
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random()*colors.length)];
+
+var favorite = 'red';
+var message;
+ // TODO: change this to your favorite color from the list
+
+// TODO: Create a block of if/else statements to check for every color except indigo and violet.
+// TODO: When a color is encountered log a message that tells the color, and an object of that color.
+//       Example: Blue is the color of the sky.
+
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+// TODO: Using the ternary operator, conditionally log a statement that
+//       says whether the random color matches your favorite color.
+if (randomColor=='red') {
+	message = 'Apples are red';
+} else if (randomColor ==' orange') {
+	message = 'basketballs are orange'; 
+} else if (randomColor == 'yellow'); {
+	message = 'bananas are yellow';
+} else if (randomColor == 'green'); {
+	message = 'grass is green';
+} else if (randomColor == 'blue'); {
+	message = 'sky is blue';
+}
+
+console.log(message);
+
+message = (randomColor == favorite) ? 'Random color matches'
+
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+switch (color) {
+
+    // todo: create a case statement that will handle every color except indigo and violet
+    // todo: when a color is encountered log a message that tells the color, and an object of that color
+    //       example: Blue is the color of the sky.
+
+    // todo: create a default case that will catch indigo and violet
+    // todo: for the default case, log: I do not know anything by that color.
+}
+
+
+
+
